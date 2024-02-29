963d19197b0b998c5e1ada24d74154651376d4bd
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2024-02-29 09:51:33 -0500

    Creating image with text component

--- wol-blog-cms-2.0/src/snippets/ImageSnippet.js
+++ wol-blog-cms-2.0/src/snippets/ImageSnippet.js
@@ -1,89 +1,76 @@
 const ImageSnippet = (elements, imagesPath) => {
-  // console.log("ImageSnippet", elements, imagesPath);
+  console.log("ImageSnippet", elements);
+  const altText = elements.altText?.value || "";
+  const desktopHeight = elements.desktopAttributes?.value?.height || "";
+  const mobileHeight = elements.mobileAttributes?.value?.height || "";
+  const desktopWidth = elements.desktopAttributes?.value?.width || "";
+  const mobileWidth = elements.mobileAttributes?.value?.width || "";
+  const desktopImage = elements.desktopImage?.value?.split(".")[0];
+  const mobileImage = elements.mobileImage?.value?.split(".")[0];
+  const imageWidth = elements.imageWidth?.value || "";
+  let modifierClasses = "";
+  switch (imageWidth) {
+    case "Wide":
+      modifierClasses = "section--wide ";
+      break;
+    case "Very wide":
+      modifierClasses = "section--very-wide";
+      break;
+    case "Full-width":
+      modifierClasses = "section--full-width";
+      break;
+    default:
+      modifierClasses = "";
+  }
 
-  // const altText = elements.altText?.value || "";
-  // const desktopHeight = elements.desktopAttributes?.value?.height || "";
-  // const mobileHeight = elements.mobileAttributes?.value?.height || "";
-  // const desktopWidth = elements.desktopAttributes?.value?.width || "";
-  // const mobileWidth = elements.mobileAttributes?.value?.width || "";
-  // const desktopImage = elements.desktopImage?.value?.split(".")[0];
-  // const mobileImage = elements.mobileImage?.value?.split(".")[0];
+  return `
 
-  // let mobileReversed = elements.mobileReversed?.value || false;
-  // let desktopReversed = elements.desktopReversed?.value || false;
-  // let modifierClasses = "";
+  <!-- Image -->
 
-  // mobileReversed = mobileReversed === "true";
-  // desktopReversed = desktopReversed === "true";
-
-  // if (mobileReversed) {
-  //   modifierClasses += " box-container--mobile-reverse";
-  // }
-  // if (desktopReversed) {
-  //   modifierClasses += " box-container--desktop-reverse";
-  // }
-
-  // const quote = elements.text.value || "";
-  //   return `
-  //   <!-- Image with quote -->
-
-  //   <section class="box-container wide anim-on-scroll ${modifierClasses}">
-  //     <div class="half-block">
-  //       <picture>
-  //         <source
-  //           srcset="${imagesPath}${mobileImage}.avif?$staticlink$"
-  //           type="image/avif"
-  //           media="(max-width: 640px)"
-  //           width="${mobileWidth}"
-  //           height="${mobileHeight}"
-  //         />
-  //         <source
-  //           srcset="${imagesPath}${mobileImage}.webp?$staticlink$"
-  //           type="image/webp"
-  //           media="(max-width: 640px)"
-  //           width="${mobileWidth}"
-  //           height="${mobileHeight}"
-  //         />
-  //         <source
-  //           srcset="${imagesPath}${mobileImage}.jpg?$staticlink$"
-  //           type="image/jpeg"
-  //           media="(max-width: 640px)"
-  //           width="${mobileWidth}"
-  //           height="${mobileHeight}"
-  //         />
-  //         <source
-  //           srcset="${imagesPath}${desktopImage}.avif?$staticlink$"
-  //           type="image/avif"
-  //           media="(min-width: 641px)"
-  //           width="${desktopWidth}"
-  //           height="${desktopHeight}"
-  //         />
-  //         <source
-  //           srcset="${imagesPath}${desktopImage}.webp?$staticlink$"
-  //           type="image/webp"
-  //           media="(min-width: 641px)"
-  //           width="${desktopWidth}"
-  //           height="${desktopHeight}"
-  //         />
-  //         <img
-  //           src="${imagesPath}${desktopImage}.jpg?$staticlink$"
-  //           alt="${altText}"
-  //           width="${desktopWidth}"
-  //           height="${desktopHeight}"
-  //         />
-  //       </picture>
-  //     </div>
-  //     <div class="half-block">
-  //         <p class="quote">
-  //           ${quote}
-  //         </p>
-  //     </div>
-  // </section>
-
-  //   `;
-  return `<!-- Image -->
-  <section class="box-container wide anim-on-scroll">
-    <h2>Image</h2>
+  <section class="${modifierClasses} anim-on-scroll">
+    <picture>
+      <source
+        srcset="${imagesPath}${mobileImage}.avif?$staticlink$"
+        type="image/avif"
+        media="(max-width: 640px)"
+        width="${mobileWidth}"
+        height="${mobileHeight}"
+      />
+      <source
+        srcset="${imagesPath}${mobileImage}.webp?$staticlink$"
+        type="image/webp"
+        media="(max-width: 640px)"
+        width="${mobileWidth}"
+        height="${mobileHeight}"
+      />
+      <source
+        srcset="${imagesPath}${mobileImage}.jpg?$staticlink$"
+        type="image/jpeg"
+        media="(max-width: 640px)"
+        width="${mobileWidth}"
+        height="${mobileHeight}"
+      />
+      <source
+        srcset="${imagesPath}${desktopImage}.avif?$staticlink$"
+        type="image/avif"
+        media="(min-width: 641px)"
+        width="${desktopWidth}"
+        height="${desktopHeight}"
+      />
+      <source
+        srcset="${imagesPath}${desktopImage}.webp?$staticlink$"
+        type="image/webp"
+        media="(min-width: 641px)"
+        width="${desktopWidth}"
+        height="${desktopHeight}"
+      />
+      <img
+        src="${imagesPath}${desktopImage}.jpg?$staticlink$"
+        alt="${altText}"
+        width="${desktopWidth}"
+        height="${desktopHeight}"
+      />
+    </picture>
   </section>
   `;
 };
