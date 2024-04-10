c7fcd3fb76750233cadff837e12bef80ef9ae726
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2024-04-10 14:06:26 -0400

    disabled TS checks

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
-
-module.exports = nextConfig;
