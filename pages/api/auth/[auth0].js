710937248016dc5aca348c3a2536dbf825b1e765
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2024-04-11 15:54:17 -0400

    testing endpoint

--- pages/api/auth/[auth0].js
+++ pages/api/auth/[auth0].js
@@ -1,3 +1,8 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+// import { handleAuth } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+// export default handleAuth();
+
+// pages/api/test.js
+export default function handler(req, res) {
+  res.status(200).json({ message: "API is working!" });
+}
