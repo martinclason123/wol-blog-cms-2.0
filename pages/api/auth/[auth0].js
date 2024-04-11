8409358e770e6b78f192db9cda9b94ff3c361f85
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2024-04-11 16:15:17 -0400

    error handling on endpoint

--- pages/api/auth/[auth0].js
+++ pages/api/auth/[auth0].js
@@ -1,8 +1,12 @@
-// import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-// export default handleAuth();
-
-// pages/api/test.js
-export default function handler(req, res) {
-  res.status(200).json({ message: "API is working!" });
-}
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res);
+    } catch (error) {
+      console.error("Login error:", error);
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+});
